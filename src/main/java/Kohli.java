6dbfcc6955ade7a441344fcd435623fca0fb0271
Rishi7830 <e1123021@u.nus.edu>
2025-01-21 10:33:56 +0800

    Incement Level 0. Rename, Greet, Exit

---
+++ src/main/java/Kohli.java
@@ -0,0 +1,14 @@
+public class Nova {
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Nova \n What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Display exit message
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
+
