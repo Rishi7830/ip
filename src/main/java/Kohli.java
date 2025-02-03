f4c9b70001c17d09a171d18701addb0cb8684f0c
Rishi7830 <e1123021@u.nus.edu>
2025-02-03 11:11:49 +0800

    Level 1. Echo

--- src/main/java/Kohli.java
+++ src/main/java/Kohli.java
@@ -1,14 +1,36 @@
-public class Nova {
+import java.util.Scanner;
+
+public class Kohli {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        // Display welcome message
+        // Display the Kohli welcome message
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Nova \n What can I do for you?");
+        System.out.println("Hello! I'm Kohli\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
 
-        // Display exit message
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+            // Read user input
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                // Exit the chatbot
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                // Echo user input
+                System.out.println("____________________________________________________________");
+                System.out.println(userInput);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
+
+
 
