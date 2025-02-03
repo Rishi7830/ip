8e576fcddbf7c39cee56e0e67bdf97c80315f5f7
Rishi7830 <e1123021@u.nus.edu>
2025-02-03 11:15:14 +0800

    Increment Level 2. Add, List

--- src/main/java/Kohli.java
+++ src/main/java/Kohli.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Kohli {
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,11 +23,26 @@ public class Kohli {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                // Display the stored tasks
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                // Echo user input
-                System.out.println("____________________________________________________________");
-                System.out.println(userInput);
-                System.out.println("____________________________________________________________");
+                // Store user input as a task
+                if (taskCount < MAX_TASKS) {
+                    tasks[taskCount] = userInput;
+                    taskCount++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + userInput);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Task list is full!");
+                    System.out.println("____________________________________________________________");
+                }
             }
         }
 
