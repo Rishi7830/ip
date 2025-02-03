ecf9aa16e6907d1c3bdedac14ff39d1dd731bd5c
Rishi7830 <e1123021@u.nus.edu>
2025-02-03 20:38:47 +0800

    Added Design :)

--- src/main/java/Kohli.java
+++ src/main/java/Kohli.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
 
-public class Kohli {
-    private static final int MAX_TASKS = 100;
-    private static Task[] tasks = new Task[MAX_TASKS];
-    private static int taskCount = 0;
+    public class Kohli {
+        private static final int MAX_TASKS = 100;
+        private static Task[] tasks = new Task[MAX_TASKS];
+        private static int taskCount = 0;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Kohli\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
+            String logo = "  _  ___   ____  _      ___ \n"
+                    + " | |/ (_) |  _ \\| |    |_ _|\n"
+                    + " | ' / _  | | | | |     | | \n"
+                    + " | . \\| | | |_| | |___  | | \n"
+                    + " |_|\\_\\_| |____/|_____| |___|\n";
+
+            String man = " O  \n"
+                    +   " /|\\ \n"
+                    +   " / \\ \n";
+
+            System.out.println(logo);
+            System.out.println(man);
+            System.out.println("____________________________________________________________");
+            System.out.println("Hello! I'm Kohli\nWhat can I do for you?");
+            System.out.println("____________________________________________________________");
 
         while (true) {
             String userInput = scanner.nextLine();
@@ -19,12 +31,12 @@ public class Kohli {
 
             if (command.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("Bye. Hope to see you again soon! and lets complete more tasks together :)");
                 System.out.println("____________________________________________________________");
                 break;
             } else if (command.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list:");
+                System.out.println("Here are the tasks in your to-do-list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
@@ -34,7 +46,7 @@ public class Kohli {
                 if (index >= 0 && index < taskCount) {
                     tasks[index].markAsDone();
                     System.out.println("____________________________________________________________");
-                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("Nice! Great Job! I've marked this task as done:");
                     System.out.println("  " + tasks[index]);
                     System.out.println("____________________________________________________________");
                 }
@@ -43,7 +55,7 @@ public class Kohli {
                 if (index >= 0 && index < taskCount) {
                     tasks[index].unmarkAsDone();
                     System.out.println("____________________________________________________________");
-                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("OK why did you mark it as done :(, Dont Worry I've marked this task as not done yet:");
                     System.out.println("  " + tasks[index]);
                     System.out.println("____________________________________________________________");
                 }
@@ -56,7 +68,7 @@ public class Kohli {
                     System.out.println("____________________________________________________________");
                 } else {
                     System.out.println("____________________________________________________________");
-                    System.out.println("Task list is full!");
+                    System.out.println("Oh no! Your Task list is full! You got too many tasks! Complete and Focus ;)");
                     System.out.println("____________________________________________________________");
                 }
             }
