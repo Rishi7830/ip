95846cbaf61dbc522491d9109aaa16ee96f38aad
Rishi7830 <e1123021@u.nus.edu>
2025-02-03 12:36:04 +0800

    New increment level 3 with task.java implementation

--- src/main/java/Kohli.java
+++ src/main/java/Kohli.java
@@ -1,88 +1,67 @@
 import java.util.Scanner;
 
 public class Kohli {
-    // Maximum number of tasks
     private static final int MAX_TASKS = 100;
-    private static String[] tasks = new String[MAX_TASKS];
-    private static boolean[] taskStatus = new boolean[MAX_TASKS];
+    private static Task[] tasks = new Task[MAX_TASKS];
     private static int taskCount = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Display the Kohli welcome message
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Kohli \nWhat can I do for you?");
+        System.out.println("Hello! I'm Kohli\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
 
         while (true) {
-            // Read the input from the user
             String userInput = scanner.nextLine();
+            String[] inputParts = userInput.split(" ", 2);
+            String command = inputParts[0];
 
-            if (userInput.equalsIgnoreCase("bye")) {
-                // User exits the chatbot
+            if (command.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else if (userInput.equalsIgnoreCase("list")) {
-                // To display the list of tasks
+            } else if (command.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    String status = taskStatus[i] ? "[X]" : "[ ]";
-                    System.out.println((i + 1) + "." + status + " " + tasks[i]);
+                    System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
-            } else if (userInput.startsWith("mark ")) {
-                // Mark the task number input by user as done
-                int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
-                if (taskIndex >= 0 && taskIndex < taskCount) {
-                    taskStatus[taskIndex] = true;
+            } else if (command.equalsIgnoreCase("mark") && inputParts.length > 1) {
+                int index = Integer.parseInt(inputParts[1]) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsDone();
                     System.out.println("____________________________________________________________");
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  [X] " + tasks[taskIndex]);
-                    System.out.println("____________________________________________________________");
-                } else {
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Invalid task number.");
+                    System.out.println("  " + tasks[index]);
                     System.out.println("____________________________________________________________");
                 }
-            } else if (userInput.startsWith("unmark ")) {
-                // To unmark the task number chosen by the user
-                int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
-                if (taskIndex >= 0 && taskIndex < taskCount) {
-                    taskStatus[taskIndex] = false;
+            } else if (command.equalsIgnoreCase("unmark") && inputParts.length > 1) {
+                int index = Integer.parseInt(inputParts[1]) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].unmarkAsDone();
                     System.out.println("____________________________________________________________");
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  [ ] " + tasks[taskIndex]);
-                    System.out.println("____________________________________________________________");
-                } else {
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Invalid task number.");
+                    System.out.println("  " + tasks[index]);
                     System.out.println("____________________________________________________________");
                 }
             } else {
-                // Adding a new task to the list as long as it is under the max tasks limit
                 if (taskCount < MAX_TASKS) {
-                    tasks[taskCount] = userInput;
-                    taskStatus[taskCount] = false; // Default: task is not done
+                    tasks[taskCount] = new Task(userInput);
                     taskCount++;
                     System.out.println("____________________________________________________________");
                     System.out.println("added: " + userInput);
                     System.out.println("____________________________________________________________");
                 } else {
                     System.out.println("____________________________________________________________");
-                    System.out.println("Task list is full. Cannot add more tasks.");
+                    System.out.println("Task list is full!");
                     System.out.println("____________________________________________________________");
                 }
             }
         }
 
-        // Closing the Scanner
         scanner.close();
     }
 }
-
-
-
