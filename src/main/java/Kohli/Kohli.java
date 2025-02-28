a3577823380e688fd9d25781e8d2223e3f433396
Rishi7830 <e1123021@u.nus.edu>
2025-02-28 11:38:57 +0800

    A-JavaDoc

--- src/main/java/Kohli/Kohli.java
+++ src/main/java/Kohli/Kohli.java
@@ -40,11 +40,11 @@ public class Kohli {
 
     private void showPossibleErrors() {
         ui.showLine();
-        System.out.println("⚠️  Possible Errors ⚠️");
-        System.out.println("❌ Missing task description (e.g., 'todo' alone is invalid)");
-        System.out.println("❌ Incorrect command format (e.g., missing /by for deadlines)");
-        System.out.println("❌ Task number out of range for mark/unmark/delete");
-        System.out.println("❌ Unrecognized command entered");
+        System.out.println(" Possible Errors ");
+        System.out.println(" Missing task description (e.g., 'todo' alone is invalid)");
+        System.out.println(" Incorrect command format (e.g., missing /by for deadlines)");
+        System.out.println(" Task number out of range for mark/unmark/delete");
+        System.out.println(" Unrecognized command entered");
         ui.showLine();
     }
 
